000ea213349808cd764c79e70b5c436bdf82c829
Albert <acsgogamer@mail.ru>
2024-08-25 20:08:24 +0300

    button

--- keyboards.py
+++ keyboards.py
@@ -5,7 +5,7 @@ from config import GAME_URL, SUPPORT_BOT_URL
 builder_main_kb = InlineKeyboardBuilder()
 
 builder_main_kb.row(types.InlineKeyboardButton(text="Играть!", url=GAME_URL))
-builder_main_kb.row(types.InlineKeyboardButton(text="❗️ВАЖНАЯ ИНФОРМАЦИЯ ДЛЯ ИГРОКОВ❗️", callback_data="info"))
+builder_main_kb.row(types.InlineKeyboardButton(text="❗️ВАЖНАЯ ИНФОРМАЦИЯ❗️", callback_data="info"))
 builder_main_kb.row(types.InlineKeyboardButton(text="Правила игры", callback_data="rules"))
 builder_main_kb.row(types.InlineKeyboardButton(text="Чат технической поддержки", url=SUPPORT_BOT_URL))
 
